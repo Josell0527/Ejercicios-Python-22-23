d6eb2488b50b0a5db78da78bd928c28fb0610f29
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-09-29 10:03:13 +0200

    Comentarios, entrada y salida de datos en Python

---
+++ Hola mundo.py
@@ -0,0 +1,16 @@
+nombre="José"
+edad= 17
+mayorOmenor= False
+
+#entrada y salida de datos
+#salida con print
+
+print("Buenos días", nombre, "tu edad es:", edad)
+
+#entrada de datos -- Input()
+nombre= input("Dime tu nombre: \n")
+
+print("Buenos días", nombre)
+
+edad= input("Dime tu edad: \n")
+print("tu edad es:", edad)
