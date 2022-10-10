e73e479ba312324cefc983ba6a9f970234abec73
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-10 11:15:41 +0200

    Agenda teléfonos
    Ejercicios

---
+++ AgendaTelefonos.py
@@ -0,0 +1,23 @@
+''' 
+Opción 1:
+LIstas para nombres 
+Listas para telefonos
+
+Opción 2:
+Listas para nombres y telefonos
+Ejemplo [Juan-Teléfono, Pepe-Teléfono]
+'''
+
+#Opción 1:
+
+vNombres= []
+vTelefonos= []
+Nombres= input("Dime un nombre:")
+Telefonos= input("Dime el teléfono de dicho nombre:")
+
+vNombres.append(Nombres)
+#vNombres.append(input("Dime un nombre:")) OTRA FORMA
+vTelefonos.append(Telefonos)
+
+print(vNombres)
+print(vTelefonos)
