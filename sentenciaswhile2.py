9cf7a035bedc90b95011eae63b7713d0fa7f8f79
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-11 14:39:27 +0200

    Contraseña
    while

---
+++ sentenciaswhile2.py
@@ -0,0 +1,9 @@
+contra= "joselito"
+valor= 0
+valor= input("Dime la contraseña: \n")
+
+while (valor !=contra ):
+    print("Contraseña incorrecta, introduce la contraseña")
+    valor= input("Dime la contraseña: \n")
+print("Contraseña correcta")
+
