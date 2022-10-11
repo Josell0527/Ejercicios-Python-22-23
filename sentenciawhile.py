0c849f0c37459c1408280861e945b5af4ba08a6b
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-11 14:39:48 +0200

    tabla de multiplicar
    while

---
+++ sentenciawhile.py
@@ -0,0 +1,11 @@
+#BUCLE WHILE
+numero= 0
+i= 0
+bandera= True
+numero = (int)(input("¿Qué número quieres saber su tabla de multiplicar?"))
+
+
+while (i<=10):
+
+    print(numero, "*", i, "=", numero*i )
+    i= i+1
