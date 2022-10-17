fee0b40d0dbe903443f575849a6a5ec78606f9c9
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-17 11:15:59 +0200

    Agendas telefonos 1

--- AgendaTelefonos.py
+++ AgendaTelefonos.py
@@ -12,11 +12,31 @@ Ejemplo [Juan-Teléfono, Pepe-Teléfono]
 
 vNombres= []
 vTelefonos= []
-Nombres= input("Dime un nombre:")
-Telefonos= input("Dime el teléfono de dicho nombre:")
+num= 0
+'''
+1-Insertar contacto
+2-Borrar contacto
+3-Buscar contacto
+4-Ver todos los contactos
+5-Salir
 
-vNombres.append(Nombres)
-#vNombres.append(input("Dime un nombre:")) OTRA FORMA
-vTelefonos.append(Telefonos)
+    if (num==1):
+        print("Insertar contacto")
+        telefono= 0
+        nombre= 0
+        nombre= input("Dime el nombre del contacto")
+        telefono= input("Dime el teléfono del contacto")
+'''
 
-print("El telefono de", (vNombres), "es", (vTelefonos))
+while (num!=5):
+    print("1-Insertar contacto")
+    print("2-Borrar contacto")
+    print("3-Buscar contacto")
+    print("4-Ver todos los contactos")
+    print("5-Salir")
+    num= (int)(print("Seleccione un número: \n")) 
+
+    try:
+        num=(int)(input("Dime un número: \n"))
+    except:
+        print("Dime un número del 1 al 5")
---
+++ Sentencias de Condicionales.py
@@ -0,0 +1,16 @@
+#Sentencias Condicionales
+
+if (5==4):
+    print("Verdadero")
+else:
+    print("Falso")
+
+    #1- Leer tu edad por teclado e imprima por pantalla si eres mayor o menor de edad
+
+edad= 0
+edad= (int) (input("Dime tu edad, \n"))
+
+if (edad>=18):
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
