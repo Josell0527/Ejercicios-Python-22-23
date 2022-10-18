717f0520d1c149ef6a5fc7522d88231fc4c005f0
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-18 14:41:44 +0200

    Agenda y ejercicios definir

--- AgendaTelefonos.py
+++ AgendaTelefonos.py
@@ -1,18 +1,3 @@
-''' 
-Opción 1:
-LIstas para nombres 
-Listas para telefonos
-
-Opción 2:
-Listas para nombres y telefonos
-Ejemplo [Juan-Teléfono, Pepe-Teléfono]
-'''
-
-#Opción 1:
-
-vNombres= []
-vTelefonos= []
-num= 0
 '''
 1-Insertar contacto
 2-Borrar contacto
@@ -27,16 +12,23 @@ num= 0
         nombre= input("Dime el nombre del contacto")
         telefono= input("Dime el teléfono del contacto")
 '''
+#Función que pinta el menú y devuelve la opción seleccionada del 1 al 5
+def pintaMenu():
+    num= 0
+    while (num< 1 or num >5 ):
+        print("**********MENÚ AGENDA*********")
+        print("1-Insertar contacto")
+        print("2-Borrar contacto")
+        print("3-Buscar contacto")
+        print("4-Ver todos los contactos")
+        print("5-Salir")
 
-while (num!=5):
-    print("1-Insertar contacto")
-    print("2-Borrar contacto")
-    print("3-Buscar contacto")
-    print("4-Ver todos los contactos")
-    print("5-Salir")
-    num= (int)(print("Seleccione un número: \n")) 
+        try:
+            num=(int)(input("Dime un número: \n"))
+        except:
+            print("Dime un número del 1 al 5")
 
-    try:
-        num=(int)(input("Dime un número: \n"))
-    except:
-        print("Dime un número del 1 al 5")
+    return num  
+print("Has seleccionado", pintaMenu())      
+
+num= pintaMenu()
---
+++ ejerciciodefinir.py
@@ -0,0 +1,6 @@
+def numerosSuma(num1:int, num2:int):
+    #print("La suma es:", (num1+num2))
+    return num1+num2
+
+suma= numerosSuma(4, 5)
+print("La suma es:", suma)
